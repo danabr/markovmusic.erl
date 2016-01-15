415bd24b45c6ccd2f545934d97984e3442d198b4
Daniel Abrahamsson <hamsson@gmail.com>
2016-01-15 23:49:45 +0100

    First stab at generating music

--- src/markovmusic.erl
+++ src/markovmusic.erl
@@ -1,6 +1,69 @@
 -module(markovmusic).
 
+-define(PREFIX_LENGTH, 2).
+
 -export([main/1]).
 
-main(_Args) ->
-  io:format("Not implemented.~n"). 
+main(Files) ->
+  Songs = load(Files),
+  {TD, Tables} = hd(analyze(Songs)),
+  [Example|_] = Tables,
+  Midi = generate_midi(TD, Example),
+  Bin = midi_generate:binary(Midi),
+  Path = "/tmp/generated.midi",
+  file:write_file(Path, Bin),
+  io:format("MIDI file written to ~s~n.", [Path]).
+
+generate_midi(TimeDivision, PrefixTable) ->
+  Events = prefix_table:generate_entries(PrefixTable),
+  {midi, {1, TimeDivision, [{track, Events}]}}.
+
+%% Internal
+load([])           -> [];
+load([File|Files]) ->
+  case midi_parse:file(File) of
+    {ok, Midi}       ->
+      [Midi|load(Files)];
+    {parse_error, _} ->
+      io:format("Failed to parse ~p.~n", [File]),
+      load(Files);
+    {error, _}       ->
+      io:format("Failed to open ~p.~n", [File]),
+      load(Files)
+  end.
+
+%% Analyze a collection of midi songs.
+%% Produces a list of probability chains, one per time division
+%% value detected in the midi files.
+analyze(Songs) ->
+  Groups = group(Songs),
+  [analyze_frequencies(Group, ?PREFIX_LENGTH) || Group <- Groups].
+
+-spec group([midi:song()]) -> [{midi:time_division(), [midi:song()]}].
+group([])                                         -> [];
+group([{midi, {_, TimeDivision, _}}=Pivot|Songs]) ->
+  Criteria = fun({midi, {_, TD, _}}) -> TD =:= TimeDivision end,
+  {Same, Different} = lists:partition(Criteria, Songs),
+  [{TimeDivision, [Pivot|Same]}|group(Different)].
+
+analyze_frequencies({TimeDivision, Songs}, PrefixLength) ->
+  Tracks = tracks(Songs),
+  Results = [analyze_track_frequencies(Track, PrefixLength) || Track <- Tracks],
+  {TimeDivision, Results}.
+
+tracks(Songs) -> lists:flatten([ music_tracks(Song) || Song <- Songs ]).
+
+music_tracks({midi, {_, _, Tracks}}) ->
+  lists:filter(fun is_music_track/1, Tracks).
+
+is_music_track({track, Events}) ->
+  Filter = fun({event, _, {note_on, _, _, _}}) -> true;
+              (_)                              -> false
+           end,
+  lists:any(Filter, Events).
+
+analyze_track_frequencies({track, Events}, PrefixLength) ->
+  Table0 = prefix_table:new(PrefixLength),
+  FoldF = fun(Event, Table) -> prefix_table:add(Table, Event) end,
+  Table = lists:foldl(FoldF, Table0, Events),
+  prefix_table:finish(Table).
---
+++ src/prefix_table.erl
@@ -0,0 +1,52 @@
+-module(prefix_table).
+
+-define(START_ENTRY, '__prefix_table_start__').
+-define(STOP_ENTRY, '__prefix_table_stop__').
+
+%% Dialyzer doesn't properly support maps, so we are using dicts.
+-opaque table(_Entry) :: {pos_integer(), list(), dict:dict()}.
+
+-export_type([table/1]).
+
+-export([ new/1
+        , add/2
+        , finish/1
+        , generate_entries/1
+        ]).
+
+-spec new(pos_integer()) -> table(_).
+new(PrefixLength) when PrefixLength > 0 ->
+  Prefix = start_prefix(PrefixLength),
+  {PrefixLength, Prefix, dict:new()}.
+
+-spec add(table(Entry), Entry) -> table(Entry).
+add({PrefixLength, Prefix0, Dict0}, Entry) ->
+  Dict = dict:append(Prefix0, Entry, Dict0),
+  Prefix = update_prefix(Prefix0, Entry),
+  {PrefixLength, Prefix, Dict}.
+
+-spec finish(table(Entry)) -> table(Entry).
+finish({PrefixLength, Prefix, Dict0}) ->
+  Dict = dict:append(Prefix, ?STOP_ENTRY, Dict0),
+  {PrefixLength, [], Dict}.
+
+-spec generate_entries(table(Entry)) -> [Entry].
+generate_entries({PL, [], Dict}) ->
+  generate_entries(Dict, start_prefix(PL));
+generate_entries({_, _, _})      ->
+  error({badarg, table_not_finished}).
+
+generate_entries(Dict, Prefix) ->
+  case generate_entry(Dict, Prefix) of
+    ?STOP_ENTRY -> [];
+    Entry       ->
+      [Entry|generate_entries(Dict, update_prefix(Prefix, Entry))]
+  end.
+
+generate_entry(Dict, Prefix) ->
+  Available = dict:fetch(Prefix, Dict),
+  Index = random:uniform(length(Available)),
+  lists:nth(Index, Available). 
+
+start_prefix(PrefixLength) -> lists:duplicate(PrefixLength, ?START_ENTRY).
+update_prefix([_|Tail], Last) -> Tail ++ [Last].
